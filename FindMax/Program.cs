54605be980e73caf7798e272b57125049e910b43
dula2000 <96098056+dula2000@users.noreply.github.com>
2023-02-05 08:00:53 +0530

    declaring the array

--- FindMax/Program.cs
+++ FindMax/Program.cs
@@ -2,7 +2,18 @@
 {
     private static void Main(string[] args)
     {
-         Random random = new Random();
-        Console.WriteLine(random.Next());
+        Console.WriteLine("Enter the size of the array input");
+        int size =int .Parse(Console.ReadLine());
+        int[] arr= new int[size];
+         Random rand = new Random();
+
+        for(int i=0; i<size; i++)
+        {
+            arr[i] = rand.Next(0, size);
+        }
+        for(int i=0; i<size; i++)
+        {
+            Console.Write(arr[i]);
+        }
     }
 }
