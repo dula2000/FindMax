44ca24c138f4c4187ce1d6309775351bb2f4d49c
dula2000 <96098056+dula2000@users.noreply.github.com>
2023-02-05 08:26:56 +0530

    Finalize the FindMax Code

--- FindMax/Program.cs
+++ FindMax/Program.cs
@@ -4,17 +4,36 @@
     {
         Console.WriteLine("Enter the size of the array input");
         int size =int .Parse(Console.ReadLine());
-        double[] arr= new double[size];
+        double[] items= new double[size];
          Random rand = new Random();
 
         for(int i=0; i<size; i++)
         {
-            arr[i] = rand.NextDouble()*100;
+            items[i] = rand.NextDouble()*100;
         }
         Console.WriteLine("The elements are in the array is : \t");
         for(int i=0; i<size; i++)
         {
-            Console.WriteLine(arr[i]);
+            Console.WriteLine(items[i]);
         }
+
+        Console.WriteLine("\n\nMax Number is in the array is " +FindMax(items,size));
+    }
+
+    static double FindMax(double[] items, int length)
+    {
+        if(length == 0) 
+            return 0;
+        //  throw new System.ArgumentException("Array is Empty");
+
+        double max = items[0];
+        for(int i=0; i<length; i++)
+        {
+            if (items[i]>max)
+            {
+                max = items[i];
+            }
+        }
+        return max; 
     }
 }
