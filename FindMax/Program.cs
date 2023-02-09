63ed8390fe3eacf54a626c7e9dcc8dc936de00c2
dula2000 <96098056+dula2000@users.noreply.github.com>
2023-02-09 11:05:14 +0530

    done

--- FindMax/Program.cs
+++ FindMax/Program.cs
@@ -9,7 +9,7 @@
 
 
         //random number generator
-        for(int i=0; i<size; i+)
+        for(int i=0; i<size; i++)
         {
             items[i] = rand.NextDouble()*100;
         }
@@ -114,6 +114,7 @@
                 {
                     maxIndex= j;
                 }
+               swap(items, j, maxIndex);
             }
         }
     }
